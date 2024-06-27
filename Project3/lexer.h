71602f694f7fd10d6a3be861b56e0396330debcf
Divanshu Chauhan <divkix@divkix.me>
2024-06-27 12:10:56 -0700

    getting 51.5/100

--- Project3/lexer.h
+++ Project3/lexer.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) Rida Bazzi, 2016
- * Edited by Joshua Elkins, 2023
  *
  * Do not share this file with anyone
  */
@@ -14,42 +13,77 @@
 
 // ------- token types -------------------
 
-typedef enum { END_OF_FILE = 0, INT, REAL, 
-   BOO, TR, FA, IF, WHILE, SWITCH, CASE, 
-   PUBLIC, PRIVATE, NUM, REALNUM, NOT, PLUS,
-   MINUS, MULT, DIV, GTEQ, GREATER, LTEQ,
-   NOTEQUAL, LESS, LPAREN, RPAREN, EQUAL,
-   COLON, COMMA, SEMICOLON, LBRACE,
-   RBRACE, ID, ERROR
+typedef enum
+{
+  END_OF_FILE,
+  INT,
+  REAL,
+  BOO,
+  TR,
+  FA,
+  IF,
+  WHILE,
+  SWITCH,
+  CASE,
+  PUBLIC,
+  PRIVATE,
+  NUM,
+  REALNUM,
+  NOT,
+  PLUS,
+  MINUS,
+  MULT,
+  DIV,
+  GTEQ,
+  GREATER,
+  LTEQ,
+  NOTEQUAL,
+  LESS,
+  LPAREN,
+  RPAREN,
+  EQUAL,
+  COLON,
+  COMMA,
+  SEMICOLON,
+  LBRACE,
+  RBRACE,
+  ID,
+  ERROR // TODO: Add labels for new token types here
 } TokenType;
 
-class Token {
-  public:
-    void Print();
+class Token
+{
+public:
+  void Print();
 
-    std::string lexeme;
-    TokenType token_type;
-    int line_no;
+  std::string lexeme;
+  TokenType token_type;
+  int line_no;
 };
 
-class LexicalAnalyzer {
-  public:
-    Token GetToken();
-    TokenType UngetToken(Token);
-    LexicalAnalyzer();
+class LexicalAnalyzer
+{
+public:
+  Token GetToken();
+  TokenType UngetToken(Token);
+  LexicalAnalyzer();
 
-  private:
-    std::vector<Token> tokens;
-    int line_no;
-    Token tmp;
-    InputBuffer input;
+private:
+  std::vector<Token> tokens;
+  int line_no;
+  Token tmp;
+  InputBuffer input;
 
-    bool SkipSpace();
-    bool SkipComments();
-    bool IsKeyword(std::string);
-    TokenType FindKeywordIndex(std::string);
-    Token ScanIdOrKeyword();
-    Token ScanNumber();
+  bool SkipSpace();
+  bool SkipComments();
+  bool IsKeyword(std::string);
+  TokenType FindKeywordIndex(std::string);
+  Token ScanIdOrKeyword();
+  Token ScanNumber();
 };
+void update_Types(int LHS, int RHS);
+void addList(std::string n, int lN, int t);
+// void printList(void);
+int Search_List(std::string n);
 
-#endif  //__LEXER__H__
+#endif //__LEXER__H__
