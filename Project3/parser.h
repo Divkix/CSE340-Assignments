8994316b89c6ad5dad08afb32ca541acef268293
Divanshu Chauhan <divkix@divkix.me>
2024-06-27 21:44:10 -0700

    format josh_parser

--- Project3/parser.h
+++ Project3/parser.h
@@ -12,32 +12,33 @@
 #include "inputbuf.h"
 #include "lexer.h"
 
-class Parser {
-	public:
-		LexicalAnalyzer lexer;
-		Token token;
-		TokenType tempTokenType;
-		int parse_program();
-	private:
-		int parse_varlist();
-		int parse_unaryOperator();
-		int parse_binaryOperator();
-		int parse_primary();
-		int parse_expression();
-		int parse_assstmt();
-		int parse_case();
-		int parse_caselist();
-		int parse_switchstmt();
-		int parse_whilestmt();
-		int parse_ifstmt();
-		int parse_stmt();
-		int parse_stmtlist();
-		int parse_body();
-		int parse_typename();
-		int parse_vardecl();
-		int parse_vardecllist();
-		int parse_globalVars();
+class Parser
+{
+public:
+	LexicalAnalyzer lexer;
+	Token token;
+	TokenType tempTokenType;
+	int parse_program();
+
+private:
+	int parse_varlist();
+	int parse_unaryOperator();
+	int parse_binaryOperator();
+	int parse_primary();
+	int parse_expression();
+	int parse_assstmt();
+	int parse_case();
+	int parse_caselist();
+	int parse_switchstmt();
+	int parse_whilestmt();
+	int parse_ifstmt();
+	int parse_stmt();
+	int parse_stmtlist();
+	int parse_body();
+	int parse_typename();
+	int parse_vardecl();
+	int parse_vardecllist();
+	int parse_globalVars();
 };
 
-#endif  //__PARSER__H__
-
+#endif //__PARSER__H__
